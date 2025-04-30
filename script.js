a4ebaaa3ed2659c886ed55d205207b939a74bd67
David Unah <davidunah022@gmail.com>
2025-04-30 12:07:02 +0100

    updated search result view

--- script.js
+++ script.js
@@ -37,10 +37,11 @@ document.getElementById('search-btn').addEventListener('click', function() {
                 </div>
             </div>
                 `
-            return movieTray
+            masterMovieBox.innerHTML = movieTray
+            
             })
 
-        masterMovieBox.innerHTML = movieTray
+        
 
         })
 })
@@ -50,9 +51,43 @@ document.getElementById('search-btn').addEventListener('click', function() {
 
 
 
+// let movieTray = ''
+// fetch("http://www.omdbapi.com/?s=kill&apikey=21488814&page=50", {
+//     method: 'GET'
+// })
+//     .then(res => res.json())
+//     .then(data => data.Search.map(movie => {
+//         movieTray +=  `
+//     <div class="each-movies">
+//         <div class="movieBanner-box">
+//             <img src="${movie.Poster}" alt="movie banner" class="movieBanner">
+//         </div>
+//         <div class="eachMovie-rightSegment">
+//             <div class="movie-NameBox">  
+//                 <h2>${movie.Title}</h2>
+//                 <img src="img/rateIcon.png" alt="rateIcon" class="ratingImage">
+//                 <p>8.0</p>
+//             </div>
+//             <div class="movie-property">
+//                 <div class="movie-minutes">
+//                     <p>164 min</p>
+//                     <p>Action, Drama, Mystery</p>
+//                 </div>
+//                 <div>
+//                     <button class="remove-Btn"><img src="img/removeIcon.png" alt="deleteIcon" class="removeBtn">Watchlist</button>
+//                 </div>
+//             </div>
+//             <p class="movie-summery">Young Blade Runner K's discovery of a long-buried 
+//                 <span> secret leads him to track down former Blade </span>
+//                 <span> Runner Rick Deckard, who's been missin...</span>
+//             </p>    
+//         </div>
+//     </div>
+//         `
+//     return movieTray
+//     }
 
-fetch("http://www.omdbapi.com/?s=kill&apikey=21488814&page=50", {
-    method: 'GET'
-})
-    .then(res => res.json())
-    .then(data => console.log(data.Search))
+// )
+    
+// )
+
