89c8dfe92d57c791b1bee4137db2df92d78867f7
David Unah <davidunah022@gmail.com>
2025-05-03 14:30:56 +0100

    update the for each method in the render movies

--- script.js
+++ script.js
@@ -3,44 +3,44 @@
 let movieTray = ''
 
 function renderMovieDetails(movieId) {
-    const masterMovieBox = document.getElementById('masterMovie-box')
     fetch(`https://www.omdbapi.com/?i=${movieId}&apikey=21488814`, {
         method: "GET"
     })
         .then(res => res.json())
-        .then(data => {
+        .then(data => data.forEach(film => { 
             movieTray +=  `
                     <div class="each-movies">
                         <div class="movieBanner-box">
-                            <img src="${data.Poster}" alt="movie banner" class="movieBanner">
+                            <img src="${film.Poster}" alt="movie banner" class="movieBanner">
                         </div>
                         <div class="eachMovie-rightSegment">
                             <div class="movie-NameBox">  
-                                <h2>${data.Title}</h2>
+                                <h2>${film.Title}</h2>
                                 <img src="img/rateIcon.png" alt="rateIcon" class="ratingImage">
-                                <p>${data.imdbRating}</p>
+                                <p>${film.imdbRating}</p>
                             </div>
                             <div class="movie-property">
                                 <div class="movie-minutes">
-                                    <p>${data.Runtime}</p>
-                                    <p>${data.Genre}</p>
+                                    <p>${film.Runtime}</p>
+                                    <p>${film.Genre}</p>
                                 </div>
                                 <div>
                                     <button class="remove-Btn"><img src="img/removeIcon.png" alt="deleteIcon" class="removeBtn">Watchlist</button>
                                 </div>
                             </div>
-                            <p class="movie-summery">${data.Plot}</p>    
+                            <p class="movie-summery">${film.Plot}</p>    
                         </div>
                     </div>
                         `
-           return masterMovieBox.innerHtml = movieTray             
+           return document.getElementById('masterMovie-box').innerHtml = movieTray             
         })
+    )
 }
 
 
 document.getElementById('search-btn').addEventListener('click', function() {
     const searchItem = document.getElementById('search-box')
-    fetch(`https://www.omdbapi.com/?s=${searchItem.value}&apikey=21488814&page=50`, {
+    fetch(`https://www.omdbapi.com/?s=${searchItem.value}&apikey=21488814&page=1`, {
         method: 'GET'
     })
         .then(res => res.json())
