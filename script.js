08fdc5dc16928a2f1c4247f2c4e664c1fdd90391
David Unah <davidunah022@gmail.com>
2025-05-03 14:08:39 +0100

    updated add a fetch to get all details

--- script.js
+++ script.js
@@ -10,39 +10,11 @@ document.getElementById('search-btn').addEventListener('click', function() {
     })
         .then(res => res.json())
         .then(data => data.Search.forEach(movie => {
-            movieTray +=  `
-        <div class="each-movies">
-            <div class="movieBanner-box">
-                <img src="${movie.Poster}" alt="movie banner" class="movieBanner">
-            </div>
-            <div class="eachMovie-rightSegment">
-                <div class="movie-NameBox">  
-                    <h2>${movie.Title}</h2>
-                    <img src="img/rateIcon.png" alt="rateIcon" class="ratingImage">
-                    <p>8.0</p>
-                </div>
-                <div class="movie-property">
-                    <div class="movie-minutes">
-                        <p>164 min</p>
-                        <p>Action, Drama, Mystery</p>
-                    </div>
-                    <div>
-                        <button class="remove-Btn"><img src="img/removeIcon.png" alt="deleteIcon" class="removeBtn">Watchlist</button>
-                    </div>
-                </div>
-                <p class="movie-summery">Young Blade Runner K's discovery of a long-buried 
-                    <span> secret leads him to track down former Blade </span>
-                    <span> Runner Rick Deckard, who's been missin...</span>
-                </p>    
-            </div>
-        </div>
-            `
-        return masterMovieBox.innerHTML = movieTray
-        }
-        
+            renderMovieDetails(movie.imdbID)
+        }  
     )
+    
 )
-
 searchItem.value = ''
 })
 
@@ -52,42 +24,49 @@ searchItem.value = ''
 
 
 // let movieTray = ''
-// fetch("https://www.omdbapi.com/?s=legend&apikey=21488814&page=50", {
+// fetch("https://www.omdbapi.com/?s=kill&apikey=21488814&page=1", {
 //     method: 'GET'
 // })
 //     .then(res => res.json())
-//     .then(data => data.Search.forEach(movie => {
-//         movieTray +=  `
-//     <div class="each-movies">
-//         <div class="movieBanner-box">
-//             <img src="${movie.Poster}" alt="movie banner" class="movieBanner">
-//         </div>
-//         <div class="eachMovie-rightSegment">
-//             <div class="movie-NameBox">  
-//                 <h2>${movie.Title}</h2>
-//                 <img src="img/rateIcon.png" alt="rateIcon" class="ratingImage">
-//                 <p>8.0</p>
-//             </div>
-//             <div class="movie-property">
-//                 <div class="movie-minutes">
-//                     <p>164 min</p>
-//                     <p>Action, Drama, Mystery</p>
-//                 </div>
-//                 <div>
-//                     <button class="remove-Btn"><img src="img/removeIcon.png" alt="deleteIcon" class="removeBtn">Watchlist</button>
-//                 </div>
-//             </div>
-//             <p class="movie-summery">Young Blade Runner K's discovery of a long-buried 
-//                 <span> secret leads him to track down former Blade </span>
-//                 <span> Runner Rick Deckard, who's been missin...</span>
-//             </p>    
-//         </div>
-//     </div>
-//         `
-//         console.log(movieTray)
+//     .then(data => {data.Search.forEach(movie => {
+//         renderMovieDetails(movie.imdbID)
 //     }
     
-// )
+// )}
 
 // )
 
+
+function renderMovieDetails(movieId) {
+    fetch(`https://www.omdbapi.com/?i=${movieId}&apikey=21488814`, {
+        method: "GET"
+    })
+        .then(res => res.json())
+        .then(data => {
+            movieTray +=  `
+                    <div class="each-movies">
+                        <div class="movieBanner-box">
+                            <img src="${data.Poster}" alt="movie banner" class="movieBanner">
+                        </div>
+                        <div class="eachMovie-rightSegment">
+                            <div class="movie-NameBox">  
+                                <h2>${data.Title}</h2>
+                                <img src="img/rateIcon.png" alt="rateIcon" class="ratingImage">
+                                <p>${data.imdbRating}</p>
+                            </div>
+                            <div class="movie-property">
+                                <div class="movie-minutes">
+                                    <p>${data.Runtime}</p>
+                                    <p>${data.Genre}</p>
+                                </div>
+                                <div>
+                                    <button class="remove-Btn"><img src="img/removeIcon.png" alt="deleteIcon" class="removeBtn">Watchlist</button>
+                                </div>
+                            </div>
+                            <p class="movie-summery">${data.Plot}</p>    
+                        </div>
+                    </div>
+                        `
+           return masterMovieBox.innerHtml = movieTray             
+        })
+}
